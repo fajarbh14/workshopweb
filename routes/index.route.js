0d7f55de1b83814b7b2aef1aa0b2490ce7163c5d
fajarbh <hajargak@gmail.com>
2022-06-10 14:20:05 +0700

    First commit

---
+++ routes/index.route.js
@@ -0,0 +1,8 @@
+const indexController = require('../controllers/index.controller');
+const auth = require('../middlewares/auth');
+
+const router = require('express').Router();
+
+router.get('/', indexController.index);
+
+module.exports = router;
